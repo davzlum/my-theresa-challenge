f3bf8b912d01dc2d8ef9b88e72b8c4bb36c6fea3
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-07 15:36:27 +0200

    Create action to get data from a movie with id

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import loadMovies from '../../redux/actions/actionCreators';
+import { loadMovies } from '../../redux/actions/actionCreators';
 import Carousel from '../Carousel';
 
 const Dashboard = () => {
--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -3,7 +3,7 @@ import actionTypes from './actionTypes';
 
 const API_KEY = '676fcc92f08e6b8bd969d6b857cce0c4';
 
-export default function loadMovies(category) {
+export function loadMovies(category) {
   return async (dispatch) => {
     try {
       const { data } = await axios(`https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&language=en-US&page=1`);
@@ -15,6 +15,22 @@ export default function loadMovies(category) {
     } catch (error) {
       dispatch({
         type: actionTypes.LOAD_MOVIES_ERROR,
+      });
+    }
+  };
+}
+
+export function loadMovie(movieId) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`);
+      dispatch({
+        type: actionTypes.LOAD_MOVIE,
+        movie: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.LOAD_MOVIE_ERROR,
       });
     }
   };
