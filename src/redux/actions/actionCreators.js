dadad5e23faf174b63c9bb5ab82f26f890638233
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-07 12:34:21 +0200

    Create action to load movies from api

---
+++ src/redux/actions/actionCreators.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const API_KEY = '676fcc92f08e6b8bd969d6b857cce0c4';
+
+export default function loadMovies(category) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios(`https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&language=en-US&page=1`);
+      dispatch({
+        type: actionTypes.LOAD_MOVIES,
+        movies: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.LOAD_MOVIES_ERROR,
+      });
+    }
+  };
+}
