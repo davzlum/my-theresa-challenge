693766c8de29850ee7a85653225ae96ae6f85e3d
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-07 15:31:57 +0200

    Get movie id on detail component

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Detail = () => (
-  <h1>Detail</h1>
-);
+const Detail = () => {
+  const { movieId } = useParams();
+  console.log(movieId);
+  return (
+    <h1>Detail</h1>
+
+  );
+};
 
 export default Detail;
