631ce316e78ae95fd7bbfdc9010aa8b48b84d20f
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-08 17:34:46 +0200

    Test fro app and index component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+/* eslint-disable react/react-in-jsx-scope */
+import './utils/matchMedia.mock';
+import { render, screen } from './utils/testUtils';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
+  window.crypto = jest.fn();
+  window.crypto.getRandomValues = jest.fn();
+  window.getComputedStyle = (eletm, select) => getComputedStyle(eletm, select);
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/collection/i);
   expect(linkElement).toBeInTheDocument();
 });
