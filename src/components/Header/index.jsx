79516f45c9564f1e155f02017e5c7fc2f5f28340
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-09 21:54:48 +0200

    Change logo and fix header position

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import './style.scss';
 function Header() {
   return (
     <header className="header">
-      <h1 className="header-logo">DataMovie</h1>
+      <h1 className="header-logo">DataMovies APP</h1>
       <nav className="header-nav">
         <button className="header-nav__button" type="button"><NavLink exact activeStyle={{ color: '#ffe300' }} to="/">Dashboard</NavLink></button>
         <button className="header-nav__button" type="button"><NavLink activeStyle={{ color: '#ffe300' }} to="/wishlist">Wishlist</NavLink></button>
